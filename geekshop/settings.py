1b6ce3a896933d9dc7b738a7ad9418f49329aca5
tdm <dtanukov@gmail.com>
2021-10-12 19:36:03 +0500

    publication

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -27,7 +27,7 @@ load_dotenv(BASE_DIR / '.env')
 DEBUG = True
 # DEBUG = False  # тестирование исключений
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -86,12 +86,20 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'geekshop',
+        'USER': 'postgres',
     }
 }
+
 
 
 # Password validation
